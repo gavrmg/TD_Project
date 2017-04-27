40015ec2b2c38d49f39bc65ea062f55f66365eba
gavrmg <gavr.27051998@mail.ru>
2017-04-27 21:33:38 +0300

    Add files via upload

---
+++ TD/desktop/src/ru/tdproject/td/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.tdproject.td.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.tdproject.td.TD_Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TD_Game(), config);
+	}
+}
