cb4438cda15289f02ec4ac97fe527425bc5e8c89
gavrmg <gavr.27051998@mail.ru>
2017-04-29 14:47:30 +0300

    Add files via upload

--- TD/core/src/ru/tdproject/td/TestLogic.java
+++ TD/core/src/ru/tdproject/td/TestLogic.java
@@ -6,16 +6,23 @@ public class TestLogic {
 	private Vector2 Pos = new Vector2(200, 200);
 	private Vector2 V = new Vector2();
 	private float t = 0;
+	Object lock = new Object();
 
-	synchronized public void move() {
-		V.x = 10 * MathUtils.cos(t);
-		V.y = 10 * MathUtils.sin(t);
-		Pos.x += V.x;
-		Pos.y += V.y;
-		t += 0.1;
+	public void move() {
+		synchronized (lock) {
+			V.x = 10 * MathUtils.cos(t);
+			V.y = 10 * MathUtils.sin(t);
+			Pos.x += V.x;
+			Pos.y += V.y;
+			t += 0.1;
+			
+		}
 	};
 
 	synchronized public Vector2 getPos() {
-		return Pos;
+		synchronized (lock) {
+			return Pos;
+			
+		}
 	}
 }
