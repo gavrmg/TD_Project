bddd1d774651f94e768fb5368ecc50b3a2b47046
misha <misha@orion-pc>
2017-06-18 17:46:48 +0300

    Added prohibition for creating towers on top of other towers and units. ObjectSearchQuery class should be move from TDInputHandler to utils.

--- TD/core/src/ru/tdproject/td/game/TDInputHandler.java
+++ TD/core/src/ru/tdproject/td/game/TDInputHandler.java
@@ -1,12 +1,18 @@
 package ru.tdproject.td.game;
+import java.util.Iterator;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.QueryCallback;
+import com.badlogic.gdx.physics.box2d.RayCastCallback;
 
 import ru.tdproject.td.TD_Game;
+import ru.tdproject.td.Objects.BaseObject;
 public class TDInputHandler implements InputProcessor  {
 	private TDWorld world;
 	private TD_Game _game;
@@ -14,29 +20,52 @@ public class TDInputHandler implements InputProcessor  {
 	private Vector2 prevPosition;
 	private Vector3 currentPosition;
 	Buttons _buttons = new Buttons();
-	Keys _keys = new Keys();
 	public Vector2 Delta;
+	private Iterator<BaseObject> iter;
+	private BaseObject o;
+	private Vector2 RayEnd;
+	private Vector2 RayStart;
+	private Vector2 UnitRadius;
+	private ifUnitNear ObjectSearchQuery = new ifUnitNear();
+	
+	private class ifUnitNear implements QueryCallback {
+		private Vector2 point = new Vector2(0,0);
+		private boolean FixtureHit = false;
+		@Override
+		public boolean reportFixture(Fixture fixture) {
+			if (fixture.isSensor())
+				return true;
+			else{
+				FixtureHit = true;
+				return false;
+			}
+		}
+		
+	}
 	public TDInputHandler(TDWorld world,TD_Game _game, TDContext _context) {
 		this.world = world;
 		this._game = _game;
 		this._context = _context;
 		this.currentPosition = new Vector3(0,0,0);
 		this.Delta = new Vector2(0,0);
+		this.RayStart = new Vector2(0,0);
+		this.RayEnd = new Vector2(0,0);
+		this.UnitRadius = new Vector2(0,0);
 		Gdx.input.setInputProcessor(this);
 	}
 	
 	@Override
 	public boolean keyDown(int keycode) {
-		if (keycode == _keys.W){
+		if (keycode == Keys.W){
 			Delta.set(0,1);
 		}
-		if (keycode == _keys.A){
+		if (keycode == Keys.A){
 			Delta.set(-1,0);
 		}
-		if (keycode == _keys.S){
+		if (keycode == Keys.S){
 			Delta.set(0,-1);
 		}
-		if (keycode == _keys.D){
+		if (keycode == Keys.D){
 			Delta.set(1,0);
 		}
 		return false;
@@ -44,7 +73,7 @@ public class TDInputHandler implements InputProcessor  {
 
 	@Override
 	public boolean keyUp(int keycode) {
-		if (keycode == _keys.W||keycode == _keys.A||keycode == _keys.S||keycode == _keys.D){
+		if (keycode == Keys.W||keycode == Keys.A||keycode == Keys.S||keycode == Keys.D){
 			Delta.set(0,0);
 		}
 		return false;
@@ -59,9 +88,14 @@ public class TDInputHandler implements InputProcessor  {
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		if (button == Buttons.LEFT){
 			currentPosition.set((float) screenX, (float)screenY, 0);
-			System.out.println(currentPosition.x+" "+(_context.HEIGHT - currentPosition.y));
 			(currentPosition.set(_game.getCamera().unproject(currentPosition))).scl(1f/TDContext.PIX_TO_METER);
-			world.createTower(5, world.TowerImg, 20, 5, 0, currentPosition.x, currentPosition.y);
+			iter = world.getUnits().iterator();
+			RayStart.set(currentPosition.x, currentPosition.y);
+			UnitRadius.set(5f,5f);
+			world.getWorld().QueryAABB(ObjectSearchQuery, currentPosition.x-UnitRadius.x, currentPosition.y-UnitRadius.y, currentPosition.x+UnitRadius.x, currentPosition.y+UnitRadius.y);
+			if (!ObjectSearchQuery.FixtureHit)
+				world.createTower(5, world.TowerImg, 20, 5, 0, currentPosition.x, currentPosition.y);
+			ObjectSearchQuery.FixtureHit = false;
 		}
 		return true;
 	}
